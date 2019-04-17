dab0ef2ae33b7e2993f228947c6cfb0d3ab32c11
rocknegi <rocknegi53@gmail.com>
2019-04-17 22:47:06 +0530

    welcome to lottie-react-native

--- App.js
+++ App.js
@@ -4,14 +4,15 @@ if(__DEV__) {
 import Reactotron from 'reactotron-react-native'
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import RotatingEarth from './components/RotatingEarth';
 
 export default class App extends Component {
   render() {
     Reactotron.log({ numbers: [1, 2, 3], boolean: false, nested: { here: 'we go' } })
     return (
       <View >
-        <Text>hello!!</Text>
-      </View>
+       <RotatingEarth />
+            </View>
     );
   }
 }
