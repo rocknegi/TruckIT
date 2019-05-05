ea3e7c53fc5cf1762676fbf5ba4f202f17b1ede0
rocknegi <rocknegi53@gmail.com>
2019-05-05 15:58:30 +0530

    update gradle and MyCustomTheme

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {View,StyleSheet,StatusBar } from 'react-native';
 import RotatingEarth from './RotatingEarth';
-import { Container, Content, StyleProvider, Button, Text } from 'native-base';
+import { Container, Content, StyleProvider, Button, Text, Footer, FooterTab } from 'native-base';
 import getTheme from '../native-base-theme/components';
 import material from '../native-base-theme/variables/material';
 import * as Animatable from 'react-native-animatable';
@@ -20,19 +20,22 @@ export default class Home extends Component {
             <Text style={{fontSize:30,fontWeight:'bold',margin:40}}>
               Heavy Duty Vehical Assistant
             </Text>
-          <View style={styles.buttonContainer}>
-          <Animatable.View animation='bounceInRight' delay={500} style={styles.buttons}>
-            <Button large style={{backgroundColor:'#f4511e'}} onPress={()=>this.props.navigation.navigate('driverLogin')}>
+
+          </Content>
+          <Footer style={styles.buttonContainer}>
+    
+         <Animatable.View animation='bounceInRight' delay={500} style={styles.buttons}>
+            <Button  style={{backgroundColor:'#f4511e'}} onPress={()=>this.props.navigation.navigate('driverLogin')}>
               <Text style={{fontSize:15}}> Driver   </Text>
             </Button>
            </Animatable.View>
            <Animatable.View animation='bounceInLeft' delay={500} style={styles.buttons}>
-            <Button large style={{backgroundColor:'#f4511e'}}  onPress={()=>this.props.navigation.navigate('managerLogin')}>
+            <Button  style={{backgroundColor:'#f4511e'}}  onPress={()=>this.props.navigation.navigate('managerLogin')}>
               <Text style={{fontSize:15}}>Manager</Text>
             </Button>
            </Animatable.View>
-          </View>
-          </Content>
+  
+          </Footer>
         </Container>
       </StyleProvider>
     )
@@ -42,16 +45,13 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   buttonContainer: {
     backgroundColor: '#9575cd',
-    paddingVertical: 15,
-    marginTop: '38%',
-    flexDirection: "row",
     justifyContent: 'space-between',
-    width: "100%",
   },
   buttons:{
     color: '#e53935',
     borderRadius: 3,
     textAlign: 'center',
+    marginTop:23,
     marginBottom: 8,
     marginRight: 8,
     marginLeft: 8,
